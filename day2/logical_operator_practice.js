c14c47952b34ae4e405f2cbcd481a0477ff63054
Amisha Rawat <amisharawat05@outlook.com>
2025-09-19 13:55:32 +0530

    Performing logical operations using if-else statement

---
+++ day2/logical_operator_practice.js
@@ -0,0 +1,12 @@
+let result = (2 < 3) && (7 > 4) ;
+if (result) {
+  console.log("It is true");
+} else {
+console.log("It is false");
+}
+
+if((2 > 1)||(3 > 2)){
+  console.log("This is an if statement");
+} else {
+  console.log("This is an else statement");
+}
