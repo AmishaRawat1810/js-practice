09a5bbf80214ef63d5289ef25c1dde5445603294
Amisha Rawat <amisharawat05@outlook.com>
2025-09-19 12:41:14 +0530

    Create first_program.js

---
+++ day1/first_program.js
@@ -0,0 +1,15 @@
+console.log("hello");
+console.log(233+10);
+console.log("welcome to the deno world");
+
+const numberOfStudents = 57;
+let numberOfFemales = 13;
+let numberOfMales = 44;
+
+console.log(numberOfFemales);
+console.log(numberOfMales);
+console.log(numberOfStudents);
+
+console.log(numberOfFemales>numberOfMales);
+console.log(numberOfFemales>numberOfMales);
+console.log(numberOfFemales<=numberOfMales);
