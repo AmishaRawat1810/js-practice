755b84b5ec1f7e4fece7fbd001fdf316d83a04b2
Amisha Rawat <amisharawat05@outlook.com>
2025-09-19 14:36:04 +0530

    Working on validating whether the entered password is valid or not

--- day5/validate_password.js
+++ day5/validate_password.js
@@ -8,9 +8,6 @@ let isPasswordValid = false;
 
 for(countIndex = 0; countIndex < inputPasswordLength; countIndex++){
   console.log(inputPassword[countIndex]);
-  for(let countIndex = 0; countIndex < validPasswordInputLength; countIndex++){
-    console.log(validPasswordInput[countIndex]);
-  }
 }
 
 const prefix = "the password is :";
