941bd79dabf73b635b014ea6300e28f33e2ec05f
Amisha Rawat <amisharawat05@outlook.com>
2025-09-19 13:54:35 +0530

    Finding greater number between two numbers using ternary

---
+++ day2/greater_number_using_ternary.js
@@ -0,0 +1,6 @@
+let number1 = 34;
+let number2 = 35;
+
+let greaterNumber = ( number1 > number2 ) ? number1 : number2;
+console.log("Greater number : ",greaterNumber);
+//console.log( number1 > number2 ? number1 : number2);
