d3bb6c49eb1b16c10e9886f8c8c25b4776088076
Amisha Rawat <amisharawat05@outlook.com>
2025-09-19 14:29:19 +0530

    Checking whether the input password is valid and length of password

---
+++ day5/check_password.js
@@ -0,0 +1,7 @@
+const inputPassword = "abcd";
+const inputPasswordLength = inputPassword.length;
+const validPasswordInput = "abcdefghijklmnopqrstuvwxyz";
+
+console.log(inputPassword);
+console.log(inputPasswordLength);
+console.log(validPasswordInput);
