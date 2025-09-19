837dbeb96542267709fdccbb88d1616cdcfd3e63
Amisha Rawat <amisharawat05@outlook.com>
2025-09-19 14:11:15 +0530

    Checking whether the entered alphabet is vowel or not

---
+++ day3/is_vowel.js
@@ -0,0 +1,21 @@
+//switch
+let alphabet = "a";
+switch(alphabet){
+  case "a" :
+    console.log(alphabet,"is a vowel.");
+    break;
+  case "e" :
+    console.log(alphabet,"is a vowel.");
+    break;
+  case "i" :
+    console.log(alphabet,"is a vowel.");
+    break;
+  case "o" :
+    console.log(alphabet,"is a vowel.");
+    break;
+  case "u" :
+    console.log(alphabet,"is a vowel");
+    break;
+  default :
+    console.log(alphabet,"is a consonant.")
+}
