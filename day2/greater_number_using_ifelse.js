1895af0d9cf5c7bba986480e05bfcb36d83d3f6b
Amisha Rawat <amisharawat05@outlook.com>
2025-09-19 13:53:33 +0530

    Finding greater number between two numbers using if-else

---
+++ day2/greater_number_using_ifelse.js
@@ -0,0 +1,8 @@
+let number1 = 34;
+let number2 = 35;
+
+if( number1 > number2 ){
+  console.log("Number1",number1,"is greater than Number 2",number2);
+} else {
+  console.log("Number 2",number2,"is greater than Number 1",number1);
+}
