530b63c0b93e4c3544ad2903fd4534f9f680acfb
Amisha Rawat <amisharawat05@outlook.com>
2025-09-19 14:05:27 +0530

    Finding greater number between three numbers using if-else if-else

---
+++ day3/check_larger_number.js
@@ -0,0 +1,10 @@
+// Condition Statements : else if, switch, while, for
+//truthiness
+if (num1 > num2 && num1 > num3 ){
+  largerNumber = num1;
+} else if (num2 > num3){
+  largerNumber = num2;
+} else {
+  largerNumber = num3;
+}
+console.log(largerNumber);
