eb25563c3d97c36120ed2478ad15306cba1c25b9
Amisha Rawat <amisharawat05@outlook.com>
2025-09-19 13:09:12 +0530

    Practiced if-else flow statement

---
+++ day2/if_else_practice.js
@@ -0,0 +1,12 @@
+//Condition Statement : if/else
+console.log();
+if(true){
+  console.log("Executed when if condition : true ");
+}
+
+let age = 18;
+if (age < 18) {
+  console.log("You are a minor");
+} else {
+  console.log("You are an adult");
+}
