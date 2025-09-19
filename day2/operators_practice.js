1a1dc2f9e65824586a8b24c055a56f920161c9a6
Amisha Rawat <amisharawat05@outlook.com>
2025-09-19 13:56:46 +0530

    Performing different operations on number

---
+++ day2/operators_practice.js
@@ -0,0 +1,23 @@
+//Operators : arithmetic, unary, increment, decrement, logical operator 
+let num1 = 20;
+let num2 = 30;
+
+num1 = num1 + num2;
+console.log("After Addition :");
+console.log("num1 : ", num1 ," num2 :",num2);
+
+num1 = num1 - num2;
+console.log("After subtraction :")
+console.log("num1 : ", num1 ," num2 :",num2);
+
+num1 = num1 * num2;
+console.log("After multiplication : ");
+console.log("num1 : ", num1 ," num2 :",num2);
+
+num1 = num1 / num2;
+console.log("After division :");
+console.log("num1 : ", num1 ," num2 :",num2);
+
+num1 = num2 % num1;
+console.log("After modulus : num1 : ");
+console.log("num1 : ", num1 ," num2 :",num2);
