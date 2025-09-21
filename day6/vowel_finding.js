8fc1c5e51cc8414348685c8c2d7c814c6eda0046
Amisha Rawat <amisharawat05@outlook.com>
2025-09-22 01:24:34 +0530

    Checking whether the meadow[index] is a vowel or not

--- day6/vowel_finding.js
+++ day6/vowel_finding.js
@@ -1,15 +1,20 @@
 const meadow = "eat cake";
-const infix = "\nVowel Islands :";
-const vowelIsland = 0;
 const meadowLength = meadow.length;
+
 const islandRequirement = "aeiou";
 const islandRequirementLength = islandRequirement.length;
 
 let findingVowel = 0;
+let vowelIsland = 0;
 
 for(findingVowel = 0; findingVowel < meadowLength; findingVowel++){
-  console.log("meadow :", meadow[findingVowel] );
+  if( meadow[findingVowel] === "a" || meadow[findingVowel] === "e" || meadow[findingVowel] === "i" || meadow[findingVowel] === "o" || meadow[findingVowel] === "u" ){
+    vowelIsland = vowelIsland + 1;
+  }
 }
 
 console.log("Meadow length",meadowLength);
+console.log("Number of islands : ",vowelIsland);
+
+const infix = "\nVowel Islands :";
 console.log(meadow,infix,vowelIsland);
