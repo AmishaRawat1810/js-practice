cf8807dc1e037a4db30a12b22dd475f3f80e3dda
Amisha Rawat <amisharawat05@outlook.com>
2025-09-22 01:10:18 +0530

    Exploring the meadow from start to end

--- day6/vowel_finding.js
+++ day6/vowel_finding.js
@@ -1,8 +1,13 @@
 const meadow = "eat cake";
-const vowelIsland = 0;
 const infix = "\nVowel Islands :";
+const vowelIsland = 0;
 const meadowLength = meadow.length;
 
+let exploreMeadow = 0;
+
+for(exploreMeadow = 0; exploreMeadow < meadowLength; exploreMeadow++){
+  console.log("meadow :", meadow[exploreMeadow] );
+}
 
 console.log("Meadow length",meadowLength);
 console.log(meadow,infix,vowelIsland);
