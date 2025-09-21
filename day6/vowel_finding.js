a0b11c55b3a8fe31c04690f7ba030ccc84d43be5
Amisha Rawat <amisharawat05@outlook.com>
2025-09-22 02:10:11 +0530

    Updated : now working for all cases

--- day6/vowel_finding.js
+++ day6/vowel_finding.js
@@ -1,17 +1,23 @@
 const meadow = "eat cake";
 const meadowLength = meadow.length;
 
-const islandRequirement = "aeiou";
-const islandRequirementLength = islandRequirement.length;
-
 let isVowel = 0;
 let vowelIsland = 0;
+let insideIsland = 0;
 
 //to find if the meadow contains any vowel island
 for( isVowel = 0; isVowel < meadowLength; isVowel++ ){
   if( meadow[isVowel] === "a" || meadow[isVowel] === "e" || meadow[isVowel] === "i" || meadow[isVowel] === "o" || meadow[isVowel] === "u" ){
-    vowelIsland = vowelIsland + 1;
-    
+    if(insideIsland === 0){
+      vowelIsland = vowelIsland + 1;
+      insideIsland = 1;
+    }
+  } else {
+    insideIsland = 0;
+  }
+}
+
+/*
     //for checking whether it is a island of more than 1 vowel
     for( isIsland = isVowel + 1; isIsland < meadowLength; isIsland++ ){
       if ( meadow[isIsland] !== "a" || meadow[isIsland] !== "e" || meadow[isIsland] !== "i" || meadow[isIsland] !== "o" || meadow[isIsland] !== "u" ){
@@ -21,3 +27,4 @@ for( isVowel = 0; isVowel < meadowLength; isVowel++ ){
     }
   }
 }
+*/
