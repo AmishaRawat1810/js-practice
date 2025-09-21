a1e75a85a79d7c127fbf78bc5ed536192dbdf99c
Amisha Rawat <amisharawat05@outlook.com>
2025-09-22 01:37:53 +0530

    Finding the vowel islands in the meadow

--- day6/vowel_finding.js
+++ day6/vowel_finding.js
@@ -7,13 +7,21 @@ const islandRequirementLength = islandRequirement.length;
 let isVowel = 0;
 let vowelIsland = 0;
 
-for(isVowel = 0; isVowel < meadowLength; isVowel++){
+//to find if the meadow contains any vowel island
+for( isVowel = 0; isVowel < meadowLength; isVowel++ ){
   if( meadow[isVowel] === "a" || meadow[isVowel] === "e" || meadow[isVowel] === "i" || meadow[isVowel] === "o" || meadow[isVowel] === "u" ){
     vowelIsland = vowelIsland + 1;
+    
+    //for checking whether it is a island of more than 1 vowel
+    for( isIsland = isVowel + 1; isIsland < meadowLength; isIsland++ ){
+      if ( meadow[isIsland] !== "a" || meadow[isIsland] !== "e" || meadow[isIsland] !== "i" || meadow[isIsland] !== "o" || meadow[isIsland] !== "u" ){
+        isVowel = isIsland; //the outer loop equals to found consonant's index
+        break;
+      }
+    }
+
   }
 }
-
-console.log("Meadow length",meadowLength);
 
 const infix = "\nVowel Islands :";
 console.log(meadow,infix,vowelIsland);
