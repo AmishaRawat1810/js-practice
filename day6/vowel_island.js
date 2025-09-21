0e76017ef699ad1e7a43bfac01e85db86e8edd57
Amisha Rawat <amisharawat05@outlook.com>
2025-09-22 02:09:01 +0530

    Added the finding_vowel.js file into vowel_island

--- day6/vowel_island.js
+++ day6/vowel_island.js
@@ -1,5 +1,22 @@
-const meadow = "a e i o u";
-const vowelIsland = 5;
-const infix = "\nVowel Islands :";
+const meadow = "eat cake";
+const meadowLength = meadow.length;
 
-console.log(meadow,infix,vowelIsland);
+let isVowel = 0;
+let vowelIsland = 0;
+let insideIsland = 0;
+
+//to find if the meadow contains any vowel island
+for( isVowel = 0; isVowel < meadowLength; isVowel++ ){
+  if( meadow[isVowel] === "a" || meadow[isVowel] === "e" || meadow[isVowel] === "i" || meadow[isVowel] === "o" || meadow[isVowel] === "u" ){
+    if(insideIsland === 0){
+      vowelIsland = vowelIsland + 1;
+      insideIsland = 1;
+    }
+  } else {
+    insideIsland = 0;
+  }
+}
+
+const prefix = "Meadow :";
+const infix = "\nVowel Islands :";
+console.log(prefix,meadow,infix,vowelIsland);
