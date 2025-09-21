c6d01e250ff67536e980b3dd31da6f4bcdcb79ba
Amisha Rawat <amisharawat05@outlook.com>
2025-09-22 01:05:38 +0530

    Finding the length of the meadow

--- day6/vowel_finding.js
+++ day6/vowel_finding.js
@@ -1,1 +1,8 @@
-console.log("hello world");
+const meadow = "eat cake";
+const vowelIsland = 0;
+const infix = "\nVowel Islands :";
+const meadowLength = meadow.length;
+
+
+console.log("Meadow length",meadowLength);
+console.log(meadow,infix,vowelIsland);
