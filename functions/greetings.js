69c4de551e123414c5ddd4730cc655cb98271d5c
Amisha Rawat <amisharawat05@outlook.com>
2025-09-23 18:23:07 +0530

    Update greetings.js

--- functions/greetings.js
+++ functions/greetings.js
@@ -1,14 +1,14 @@
-function personToGreet(personName) {
-  return personName ;
+function toGreet(greetings,personName) {
+  return greetings + personName ;
 }
 
 function greeting(greetNumber) {
   switch(greetNumber) {
     case 1 : return "Good Morning,";
-    case 2 : return "Good Afternoon,";
-    case 3 : return "Good evening,";
-    case 4 : return "Good night,";
+    case 2 : return "Good Afternoon, ";
+    case 3 : return "Good evening, ";
+    case 4 : return "Good night, ";
   }
 }
 
-console.log(greeting(4), personToGreet('Yash'));
+console.log(personToGreet(greeting(3),'Yash'));
