2aa075c5c3133b351aa6ccbfc02465dbee4b132f
Amisha Rawat <amisharawat05@outlook.com>
2025-09-24 10:19:34 +0530

    Update second_largest_amoung_three.js

--- functions/second_largest_amoung_three.js
+++ functions/second_largest_amoung_three.js
@@ -1,24 +1,13 @@
-function smallestOfTwoNumber(x, y) {
+function minOfTwo(x, y) {
   return x < y ? x : y;
 }
 
-function smallestOfThreeNumber(x, y, z){
-  return smallestOfTwoNumber(smallestOfTwoNumber(x, y), z);
-}
-
-function greatestOfTwoNumber(x, y) {
+function maxOfTwo(x, y) {
   return x > y ? x : y;
 }
 
-function greatestOfThreeNumbers(a, b, c) {
-  return greatestOfTwoNumber(greatestOfTwoNumber(a, b), c);
+function secondLargestAmoungThree(x, y, z) {
+  return minOfTwo(maxOfTwo(x,y), maxOfTwo(y,z));
 }
 
-function secondLargest(a, b, c){
-  const greatest = greatestOfThreeNumbers(a, b, c);
-  const smallest = smallestOfThreeNumber(a, b, c);
-  const second = (a !== greatest && a !== smallest) ? a : ( b !== greatest && b !== smallest ? b : c );
-  return second;
-}
-
-console.log(secondLargest(5,2,3));
+console.log(secondLargestAmoungThree(3,4,5));
