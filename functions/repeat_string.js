0f3724c62e6b0b73201be36e21f14a31a2ccc5dc
Amisha Rawat <amisharawat05@outlook.com>
2025-09-25 10:04:18 +0530

    Create repeat_string.js

---
+++ functions/repeat_string.js
@@ -0,0 +1,22 @@
+function repeatString(stringToRepeat, repeatCount) {
+  let resultedString = "";
+  for (let count = repeatCount; count > 0; count--) {
+    resultedString = resultedString + stringToRepeat + " ";
+  }
+  return resultedString;
+}
+
+function testRepeatString(stringToRepeat, repeatCount, expectedValue) {
+  const resultedString = repeatString(stringToRepeat, repeatCount);
+  const checkValue = resultedString === expectedValue ? "✅" : "❌";
+
+  console.log(checkValue, resultedString, expectedValue);
+}
+
+function testAll(){
+  testRepeatString("hi",2, "hi hi ");
+  testRepeatString("bye",3, "bye bye bye ");
+  testRepeatString("ho!",4, "ho! ho! ho! ho! ");
+}
+
+testAll();
