cb58fddc11e261232e45280ef08f7ee56afe999c
Amisha Rawat <amisharawat05@outlook.com>
2025-10-14 07:03:56 +0530

    added movement logic

--- games/maze.js
+++ games/maze.js
@@ -1,25 +1,3 @@
-const maze1 = [
-  '####################'.split(''),
-  '#🙂   #         #  #'.split(''),
-  '# ## # ### #### # ##'.split(''),
-  '# #    #     #    ##'.split(''),
-  '# #### ### # #######'.split(''),
-  '#      #    #      #'.split(''),
-  '##################E#'.split('')
-]
-
-const maze2 = [
-  '##########################'.split(''),
-  '#🙂     #       #        #'.split(''),
-  '# ### ### ##### # ###### #'.split(''),
-  '#   #   #   #   # #    # #'.split(''),
-  '### # ##### # ### # ## # #'.split(''),
-  '#   #     # #     # ##   #'.split(''),
-  '# ####### # ####### ######'.split(''),
-  '#       # #       #      E'.split(''),
-  '##########################'.split('')
-]
-
 const maze3 = [
   '#####################################'.split(''),
   '# #    #       #         #     #    #'.split(''),
@@ -30,62 +8,66 @@ const maze3 = [
   '# ####### ##### ### ### ### ### #####'.split(''),
   '#       #     #     #     #   #     E'.split(''),
   '#####################################'.split('')
-]
+];
 
-function displayMaze (maze) {
+function displayMaze(maze) {
   for (let i = 0; i < maze.length; i++) {
-    let line = ''
+    let line = '';
     for (let j = 0; j < maze[i].length; j++) {
-      line += maze[i][j]
+      line += maze[i][j];
     }
-    console.log(line)
+    console.log(line);
   }
 }
 
-function playerPositionAtStart (maze) {
-  const randomIndex = Math.floor(Math.random() * 9)
+function playerPositionAtStart(maze) {
+  const randomIndex = Math.floor(Math.random() * 9);
   if (maze[randomIndex][randomIndex] !== '#') {
-    maze[randomIndex, randomIndex] = "_";
-    return [randomIndex, randomIndex]
+    maze[randomIndex][randomIndex] = '_';
+    return [randomIndex, randomIndex];
   }
-  return playerPositionAtStart(maze)
+  return playerPositionAtStart(maze);
 }
 
-function operateOnCoordinate (playerCoordinate, y, x) {
-  return [playerCoordinate[0] + y, playerCoordinate[1] + x]
+function operateOnCoordinate(playerCoordinate, y, x) {
+  return [playerCoordinate[0] + y, playerCoordinate[1] + x];
 }
 
-function updatePosition (playerCoordinate, playerMove, maze) {
+function updatePosition(playerCoordinate, playerMove, maze) {
   const moves = [
     [-1, 0],
     [1, 0],
     [0, -1],
     [0, 1]
-  ] // W, S, A, D
-  const movement = ['w', 's', 'a', 'd']
-  const [y, x] = moves[movement.indexOf(playerMove)]
-  if (maze[y, x] !== '#') {
-    const updatedPosition = operateOnCoordinate(playerCoordinate, y, x)
-    return updatedPosition
+  ]; // W, S, A, D
+  const movement = ['w', 's', 'a', 'd'];
+  const [y, x] = moves[movement.indexOf(playerMove)];
+  
+  const [newY, newX] = operateOnCoordinate(playerCoordinate, y, x);
+  console.log('Trying to move to:', newY, newX, '=>', `["${maze[newY][newX]}"]`);
+  if (maze[newY][newX] !== '#') {
+    console.log('Moved to :', newY, newX);
+    return [newY, newX];
   }
-
-  return "INVALID MOVE";
+  return playerCoordinate;
 }
 
-let playerCoordinate = playerPositionAtStart(maze3)
+let playerCoordinate = playerPositionAtStart(maze3);
 
-function movePlayer (maze, playerMove) {
-  const [row, col] = playerCoordinate
-  maze[(row, col)] = ' '
+function movePlayer(maze, playerMove) {
+  const [row, col] = playerCoordinate;
+  maze[row][col] = ' ';
 
-  const [newRow, newCol] = updatePosition(playerCoordinate, playerMove, maze)
+  const [newRow, newCol] = updatePosition(playerCoordinate, playerMove, maze);
 
-  if (maze[(newRow, newCol)] !== '#') {
-    playerCoordinate = [newRow, newCol]
+  if (maze[newRow][newCol] !== '#') {
+    playerCoordinate = [newRow, newCol];
   }
+
   const [r, c] = playerCoordinate;
-  maze[r, c] = '_';
+  maze[r][c] = '_';
 }
 
-movePlayer(maze3, 'a')
-displayMaze(maze3)
+// displayMaze(maze3);
+movePlayer(maze3, 'w');
+displayMaze(maze3);
