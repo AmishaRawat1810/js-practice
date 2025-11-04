5ff3f1b31166e4a0cff6797e54f38d867f2f0651
Amisha Rawat <amisharawat05@outlook.com>
2025-11-05 00:32:26 +0530

    Update bencode_decode.js

--- Extra-problems/bencode_decode.js
+++ Extra-problems/bencode_decode.js
@@ -1,25 +1,33 @@
 const INVALID = "Invalid";
 
-function validInteger (num) {
- return isNaN(num);
+function isInvalidNum (num) {
+  return isNaN(num);
+}
+
+function isInvalidString (input, string, length) {
+  return string.length !== length;
 }
 
 function decodeByteString (input, start) {
   const colonIndex = input.indexOf(":", start);
   const length = +(input.slice(start, colonIndex)); //gives the length of the string
-  if (validInteger(length)) {
+  if (isInvalidNum(length)) {
+    return [INVALID];
+  }
+  const end = colonIndex + 1 + length;
+  const decodedString = input.slice(colonIndex + 1, end); //slice the original string
+
+  if (isInvalidString(input, decodedString, length)) {
     return [INVALID];
   }
 
-  const end = colonIndex + 1 + length;
-  const decodedString = input.slice(colonIndex + 1, end); //slice the original string
   return [decodedString, end];
 }
 
 function decodeInteger (input, start) {
   const end = input.indexOf("e", start); // where the number ends
   const decodedInteger = +(input.slice(start + 1, end)); // slice to get the number
-  if (validInteger(decodedInteger)) {
+  if (isInvalidNum(decodedInteger)) {
     return [INVALID];
   }
   return [decodedInteger, end + 1]; //skip the 'e' as well
@@ -31,8 +39,15 @@ function decodeList (input, start) {
 
   while (input[index] !== "e") {
     const [element, nextIndex] = callDecodeFor (input[index], input, index);
+    if (element === INVALID) {
+      return [INVALID];
+    }
     decodedList.push(element);
     index = nextIndex;
+  }
+
+  if (input[index] !== "e") { //to check whether the last element is E
+    return [INVALID];
   }
 
   return [decodedList, index + 1];
@@ -50,10 +65,13 @@ function callDecodeFor (firstElement, input, start = 0) {
   if (!isNaN(parseInt(firstElement))) {
     return decodeByteString(input, start);
   }
+  return [INVALID];
 }
 
 function decode (inputValue) {
-  const [result] = callDecodeFor (inputValue[0], inputValue, 0);
+  const [result, index] = callDecodeFor (inputValue[0], inputValue, 0);
+  console.log("INdex : ",index, "INPUT VALue : ",inputValue[index - 1]);
+  
   return result;
 }
 
@@ -102,20 +120,21 @@ function testForString () {
   testCode ("Char in string : 0", "0:", "");
   testCode ("Char in string : 1", "1:a", "a");
   testCode ("Char in string : 2", "2:ab", "ab");
-  testCode ("Char in string : > 2", "4:abcd", "abcd");
   testCode ("Char in string : special", "6:%$#@!&", "%$#@!&");
+  testCode ("INVALID String : < char than length passed", "4:abcde", INVALID);
+  testCode ("INVALID String : > char than length passed", "4:abc", INVALID);
   console.log('\n');
 }
 
 function testForList () {
   heading("TEST FOR LISTS");
-  testCode ("Elements in list : 1, integer", "li1ee", [1]);
+  testCode ("Elements in list : 1, integer", "li142aee", INVALID);
   testCode ("Elements in list : 2, integer (+,+)", "li1ei2ee", [1,2]);
   testCode ("Elements in list : 2, integer (-,-)", "li-1ei-2ee", [-1,-2]);
   testCode ("Elements in list : 2, integer (-,+)", "li-1ei2ee", [-1,2]);
   testCode ("Elements in list : 1, empty string", "l0:e", [""]);
   testCode ("Elements in list : 2, string", "l1:a5:helloe", ["a", "hello"]);
-  testCode ("Elements in list : 5, string of e", "l5:eeeeee", ["eeeee"]);
+  testCode ("Invalid : length : 5, elements : 6", "l5:eeeeee", INVALID);
   testCode ("Elements in list : 2, nested string", "l1:al5:helloee", ["a", ["hello"]]);
   console.log('\n');
 }
